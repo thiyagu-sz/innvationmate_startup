791cff7b1dc62ef2df1dbe2198392c8bdd6630a6
thiyagu-sz <thiyaguai2004@gmail.com>
2025-02-23 12:19:49 +0530

    cc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,26 @@
-import React, { useEffect } from 'react';
-import Navbar from './components/Navbar';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Doctors from './pages/Expertise';
-import Login from './pages/Login';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Appointment from './pages/Appointment';
-import MyAppointments from './pages/MyAppointments';
-import MyProfile from './pages/MyProfile';
-import Footer from './components/Footer';
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Expertise'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Appointment from './pages/Appointment'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Footer from './components/Footer'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/Verify';
+import Verify from './pages/Verify'
 
 const App = () => {
-  
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "//code.tidio.co/nvzggz2d8gas8qm7vzt5bcvgwe6e4tuj.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <ToastContainer />
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home/>} />
         <Route path='/experts' element={<Doctors />} />
         <Route path='/expertise/:speciality' element={<Doctors />} />
         <Route path='/login' element={<Login />} />
@@ -41,7 +33,7 @@ const App = () => {
       </Routes>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
